1f1e96de960212a035b7654ce61bcced95a278a2
Papa-Sean <smsugrue@gmail.com>
2021-02-09 22:10:34 -0500

    Did this work?

---
+++ emp-dir-app/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "../styles/styles.css";
+
+function Header () {
+    return (
+      <div className="header">
+        <h1>BIG BROTHER... I MEAN... EMPLOYEE DIRECTORY</h1>
+        <p>Click on HEADERS to filter by heading or use the search box to narrow your results.</p>
+      </div>
+    )
+  }
+
+export default Header;
---
+++ emp-dir-app/src/components/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import DataArea from "./DataArea";
+import "../styles/styles.css";
+
+function Main () {
+    return (
+      <>
+        <DataArea />
+      </>
+    );
+  };
+
+export default Main;
